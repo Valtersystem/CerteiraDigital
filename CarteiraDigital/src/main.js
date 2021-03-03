78ee6165d91c05a0148af814ec6cee14599071ba
valtesystem <valterifsp@gmail.com>
2021-03-03 01:41:09 -0400

    Primeiro commit

---
+++ CarteiraDigital/src/main.js
@@ -0,0 +1,16 @@
+//Parando o envio do formulario
+
+document.getElementById("enviar").addEventListener("click", function(event){
+
+    let nomeCarteira = document.getElementById("titulo").Value;
+    let rendaAtual = document.getElementById("renda").textContent;
+
+    console.log(nomeCarteira)
+    event.preventDefault()
+  });
+
+// Guardando os valores de input na variaveis
+
+document.getElementById("enviar").addEventListener("click", function(event){
+   
+});
