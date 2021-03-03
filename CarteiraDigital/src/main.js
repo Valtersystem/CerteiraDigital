a5e0016e29551770bf75d808488d29d842c9e57e
valtesystem <valterifsp@gmail.com>
2021-03-03 02:22:54 -0400

    pegando os valores das variáveis

--- CarteiraDigital/src/main.js
+++ CarteiraDigital/src/main.js
@@ -1,16 +1,25 @@
-//Parando o envio do formulario
 
-document.getElementById("enviar").addEventListener("click", function(event){
+// Guardando os valores do input
+function pegaValor(){
+    let nomeCarteira = document.getElementById("titulo").value;
+    let rendaAtual = parseFloat(document.getElementById("renda").value);
+}
 
-    let nomeCarteira = document.getElementById("titulo").Value;
-    let rendaAtual = document.getElementById("renda").textContent;
 
-    console.log(nomeCarteira)
-    event.preventDefault()
-  });
+// Evento que é executado toda vez que uma tecla for pressionada no input
+document.getElementById("enviar").onkeypress = function(e) {
+    // 13 é a tecla <ENTER>. Se ela for pressionada, mostrar o valor
+    if (e.keyCode == 13) {
+        pegaValor();
+        e.preventDefault();
+    }
+}
 
-// Guardando os valores de input na variaveis
+// Evento que é executado ao clicar no botão de enviar
+document.getElementById("enviar").onclick = function(e) {
+    pegaValor();
+    e.preventDefault();
+}
 
-document.getElementById("enviar").addEventListener("click", function(event){
-   
-});
+
+
